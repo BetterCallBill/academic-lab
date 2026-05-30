ff665755334d1d4927d6174436363f58effe740d
Billy Wang <billy.wang.aus@email.com>
2026-05-30 13:20:45 +1000

    fix: update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://chenlab.pages.dev',
+  site: 'https://academic-lab.pages.dev',
   integrations: [tailwind()],
   output: 'static',
 });
